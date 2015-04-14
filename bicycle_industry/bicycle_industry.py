714227f5cdf9e2bb14fa235bbe9a78bd42e4fe45
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-04-14 22:58:56 +0400

    bicycle_industry: create project and initial code

---
+++ bicycle_industry/bicycle_industry.py
@@ -0,0 +1,16 @@
+class Bicycle(object):
+    def __init__(self, model, weight, production_cost):
+        self.model = model
+        self.weight = weight
+        self.production_cost = production_cost
+
+class BikeShop(object):
+    def __init__(self, shop_name):
+        self.shop_name = shop_name
+        self.inventory = []
+
+    def sell(self, margin_percent):
+
+    def display_profit(self):
+
+class Customers(object):
