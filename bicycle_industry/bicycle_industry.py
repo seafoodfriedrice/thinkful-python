3da148d6e393026160d5651e82fb1ea3065e4078
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-04-15 22:40:53 +0400

    bicycle_industry: finish up sell() method and fix show_inventory() method

--- bicycle_industry/bicycle_industry.py
+++ bicycle_industry/bicycle_industry.py
@@ -22,19 +22,33 @@ class BicycleShop(object):
             self.bicycle_inventory[bicycle.model].append(bicycle)
         self.store_balance = self.store_balance - bicycle.production_cost
 
-    """
-    def sell(self, bicycle, margin_percent, customer):
-        for bicycle in self.bicycle_inventory:
-            if isinstance(bicycle, Bicycle) and bicycle.name
-                             * (float(margin_percent) / 100.0))
-                self.bicycle_inventory.remove(bicycle)
-                self.store_balance = self.store_balance + sell_price
-    """
+    def sell(self, bicycle_model, customer):
+        if bicycle_model in self.bicycle_inventory:
+            if len(self.bicycle_inventory[bicycle_model]) > 0:
+                bicycle_price = self.bicycle_inventory[bicycle_model][0].price
+                if customer.budget >= bicycle_price:
+                    self.bicycle_inventory[bicycle_model].pop()
+                    self.store_balance = self.store_balance + bicycle_price
+                    customer.budget = customer.budget - bicycle_price
+                    print "\nSold {} bicycle to {} for ${}.".format(
+                        bicycle_model, customer.name, bicycle_price)
+                    print "{} has ${} left.".format(
+                        customer.name, customer.budget)
+                else:
+                    print "\nSorry, {} cannot afford the {} bicycle on their budget.".format(
+                        customer.name, bicycle_model)
+            else:
+                print "\nSorry, we don't have any {} bicycles left.".format(bicycle_model)
+        else:
+            print "\nSorry, we don't have any models called {}.".format(bicycle_model)
 
     def show_inventory(self):
         print "{} bicycle inventory:".format(self.shop_name)
         for bicycle_model, bicycle in self.bicycle_inventory.iteritems():
-            print "> ({}) {} ${}".format(len(bicycle), bicycle_model, bicycle[0].price)
+            if len(self.bicycle_inventory[bicycle_model]) > 0:
+                print "> ({}) {} ${}".format(len(bicycle), bicycle_model,
+                                             bicycle[0].price)
+            # Does not display out of stack bicycles and their prices
 
     """def display_profit(self):"""
 
@@ -43,9 +57,7 @@ class Customer(object):
         self.name = name
         self.budget = budget
         self.bicycles_owned = []
-
-    def purchase_bicycle(self, bicycle_shop, bicycle_model):
-        
+    """def purchase_bicycle(self, bicycle_shop, bicycle_model):"""
 
 my_shop = BicycleShop("Pedal Files")
 my_shop.add_bicycle(Bicycle("Green", 50, 150))
@@ -72,4 +84,8 @@ for customer in [customer_one, customer_two, customer_three]:
             print "* {}".format(bicycle[0].model)
     print
 
+my_shop.show_inventory()
+my_shop.sell("Green", customer_one)
+my_shop.sell("Red", customer_two)
+my_shop.sell("Black", customer_three)
 my_shop.show_inventory()
