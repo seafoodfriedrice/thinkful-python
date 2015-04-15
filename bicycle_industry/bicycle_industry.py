fd40e516524c16f307214d15829a6f9cdb84fb21
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-04-15 21:56:40 +0400

    bicycle_industry: add self.price and additional main code

--- bicycle_industry/bicycle_industry.py
+++ bicycle_industry/bicycle_industry.py
@@ -3,6 +3,10 @@ class Bicycle(object):
         self.model = model
         self.weight = weight
         self.production_cost = production_cost
+        self.margin_percent = 20
+        # Need to correctly display two decimal points on price
+        self.price = self.production_cost + (self.production_cost *
+                          (float(self.margin_percent) / 100))
 
 class BicycleShop(object):
     def __init__(self, shop_name):
@@ -17,12 +21,11 @@ class BicycleShop(object):
         else:
             self.bicycle_inventory[bicycle.model].append(bicycle)
         self.store_balance = self.store_balance - bicycle.production_cost
-        
+
     """
     def sell(self, bicycle, margin_percent, customer):
         for bicycle in self.bicycle_inventory:
             if isinstance(bicycle, Bicycle) and bicycle.name
-                sell_price = bicycle.production_cost + (bicycle.production_cost
                              * (float(margin_percent) / 100.0))
                 self.bicycle_inventory.remove(bicycle)
                 self.store_balance = self.store_balance + sell_price
@@ -30,9 +33,8 @@ class BicycleShop(object):
 
     def show_inventory(self):
         print "{} bicycle inventory:".format(self.shop_name)
-        for bicycle_name, bicycle in self.bicycle_inventory.iteritems():
-            # How to correctly calculate margin cost and display
-            print "> {} ({})".format(bicycle_name, len(bicycle))
+        for bicycle_model, bicycle in self.bicycle_inventory.iteritems():
+            print "> ({}) {} ${}".format(len(bicycle), bicycle_model, bicycle[0].price)
 
     """def display_profit(self):"""
 
@@ -41,9 +43,9 @@ class Customer(object):
         self.name = name
         self.budget = budget
         self.bicycles_owned = []
-    """
+
     def purchase_bicycle(self, bicycle_shop, bicycle_model):
-    """
+        
 
 my_shop = BicycleShop("Pedal Files")
 my_shop.add_bicycle(Bicycle("Green", 50, 150))
@@ -56,13 +58,18 @@ my_shop.add_bicycle(Bicycle("Silver", 125, 300))
 my_shop.add_bicycle(Bicycle("Gold", 125, 500))
 my_shop.add_bicycle(Bicycle("Black", 100, 750))
 
+print "{}'s store balance is ${}.\n".format(my_shop.shop_name, my_shop.store_balance)
+
 customer_one = Customer("Cyclops", 200)
 customer_two = Customer("Gambit", 500)
 customer_three = Customer("Wolverine", 1000)
 
 for customer in [customer_one, customer_two, customer_three]:
     print "{} has a budget of ${}.".format(customer.name,customer.budget)
-    #print "{} can afford the following bicycles from {}:".format(my_shop.shop_name)
-    print 
+    print "{} can afford the following bicycles:".format(customer.name, my_shop.shop_name)
+    for bicycle_model, bicycle in my_shop.bicycle_inventory.iteritems():
+        if customer.budget >= bicycle[0].price:
+            print "* {}".format(bicycle[0].model)
+    print
 
 my_shop.show_inventory()
