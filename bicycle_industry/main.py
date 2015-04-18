bd4799199ec8f52a1f36f55d4c698d65cbffb5c7
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-04-18 14:05:39 +0400

    bicycle_industry: achieve modular challenge with Wheel and Frame classes

--- bicycle_industry/main.py
+++ bicycle_industry/main.py
@@ -1,17 +1,10 @@
-from bicycles import *
+from bicycles_challenge import *
 
 def main():
     my_shop = BicycleShop("Pedal Files")
-    my_shop.add_bicycle(Bicycle(model="Green", weight=50, production_cost=150))
-    my_shop.add_bicycle(Bicycle(model="Green", weight=50, production_cost=150))
-    my_shop.add_bicycle(Bicycle(model="Green", weight=50, production_cost=150))
-    my_shop.add_bicycle(Bicycle(model="Blue", weight=75, production_cost=200))
-    my_shop.add_bicycle(Bicycle(model="Blue", weight=75, production_cost=200))
-    my_shop.add_bicycle(Bicycle(model="Red", weight=100, production_cost=250))
-    my_shop.add_bicycle(Bicycle(model="Silver", weight=125, production_cost=300))
-    my_shop.add_bicycle(Bicycle(model="Gold", weight=125, production_cost=500))
-    my_shop.add_bicycle(Bicycle(model="Black", weight=100, production_cost=750))
-
+    bicycles = ['Zeus', 'Zeus', 'Athena', 'Ares', 'Poseidon', 'Hermes', 'Hera']
+    for bicycle in bicycles:
+        my_shop.add_bicycle(Bicycle(bicycle))
     print "{}'s store balance is ${}.".format(my_shop.shop_name, my_shop.store_balance)
     print "{}'s store profit is ${}.\n".format(my_shop.shop_name, my_shop.profit)
 
@@ -30,16 +23,14 @@ def main():
         print
 
     my_shop.show_inventory()
-    colors = ['Green', 'Red', 'Black']
-    for color, customer in zip(colors, customers):
-        my_shop.sell(color, customer)
+
+    bicycles = ['Zeus', 'Hera', 'Ares']
+    for bicycle, customer in zip(bicycles, customers):
+        my_shop.sell(bicycle, customer)
+
     print
     my_shop.show_inventory()
     print "\n{}'s profit is now ${}.".format(my_shop.shop_name, my_shop.profit)
 
 if __name__ == '__main__':
-    f = Frame('carbon')
-    w = Wheel('cart')
-    print "Weight: {}, Production Cost: {}".format(f.weight, f.production_cost)
-    print "Weight: {}, Production Cost: {}".format(w.weight, w.production_cost)
-    #main()
+    main()
