389868303a5028030378ef61a4d14e3076dbc844
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-04-19 23:26:24 +0400

    snippets-app: create main() with parser instance

--- snippets-app/snippets.py
+++ snippets-app/snippets.py
@@ -1,4 +1,6 @@
 import logging
+import argparse
+import sys
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
@@ -21,3 +23,12 @@ def get(name):
     """
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
     return ""
+
+def main():
+    """Main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+    arguments = parser.parse_args(sys.argv[1:])
+
+if __name__ == '__main__':
+    main()
