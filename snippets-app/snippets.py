bbb1a47d48ff3af278162d35be92c661e16dbf6d
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-04-20 20:33:19 +0400

    snippets-app: add subparser for get command

--- snippets-app/snippets.py
+++ snippets-app/snippets.py
@@ -28,6 +28,20 @@ def main():
     """Main function"""
     logging.info("Constructing parser")
     parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+    
+    # Subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="The name of the snippet")
+    put_parser.add_argument("snippet", help="The snippet text")
+
+    # Subparser for the get command
+    logging.debug("Constructing get subparser")
+    get_parser = subparsers.add_parser("get", help="Retrieve a snipper")
+    get_parser.add_argument("name", help="The name of the snippet")
+
     arguments = parser.parse_args(sys.argv[1:])
 
 if __name__ == '__main__':
