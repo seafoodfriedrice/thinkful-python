e5db46838cd38464b92e72bb572cb9f6789745d7
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-04-30 22:50:03 +0400

    flask_hello_world: add view function for return a Jedi name

--- flask_hello_world/hello_world.py
+++ flask_hello_world/hello_world.py
@@ -19,5 +19,14 @@ def hello_person(name):
     """
     return html.format(name.title())
 
+@app.route("/jedi/<first>/<last>")
+def make_jedi_name(first, last):
+    jedi_name = last[0:3] + "'" + first[0:2]
+    html = """
+        <p>Your Jedi name is:</p>
+        <h1>{}</h1>
+    """
+    return html.format(jedi_name.title())
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
