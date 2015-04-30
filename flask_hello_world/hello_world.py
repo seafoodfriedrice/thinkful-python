30203b336e002fd051b1de4497002cae1510ac92
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-04-30 22:40:35 +0400

    flask_hello_world: add additional route to return name

--- flask_hello_world/hello_world.py
+++ flask_hello_world/hello_world.py
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def say_hi():
     return "Hello World"
 
+@app.route("/hello/<name>")
+def hello_person(name):
+    html = """
+        <h1>
+            Hello {}!
+        </h1>
+        <p>
+            Here's a picture of a kitten. Awww...
+        </p>
+        <img src="http://placekitten.com/g/200/300">
+    """
+    return html.format(name.title())
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
