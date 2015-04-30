50b2aa873fde32548f58805c1fc4a7e8ccda8690
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-04-30 23:51:53 +0400

    jinja_example: update to use macros

--- jinja_example/run.py
+++ jinja_example/run.py
@@ -6,5 +6,22 @@ def template_test():
     return render_template('template.html', my_string="Wheeeee!",
                            my_list=[0, 1, 2, 3, 4, 5])
 
+@app.route("/home")
+def home():
+    return render_template('template.html', my_string="I'm the home page",
+                           my_list=[0, 1, 2, 3, 4, 5])
+
+@app.route("/about")
+def about():
+    return render_template('template.html', my_string="I'm the about page",
+                           my_list=[0, 1, 2, 3, 4, 5])
+
+@app.route("/contact")
+def contact():
+    return render_template('template.html', my_string="I'm the contact page",
+                           my_list=[0, 1, 2, 3, 4, 5])
+
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
