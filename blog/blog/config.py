36f17a46cf3b17aad27c17bf4b161bb76e9a39c6
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-05-05 01:35:41 +0400

    blog: structure and initialize Building Blogful with Flask project

---
+++ blog/blog/config.py
@@ -0,0 +1,6 @@
+import os
+
+class DevelopmentConfig(object):
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/blogful"
+    DEBUG = True
+    SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", "")
