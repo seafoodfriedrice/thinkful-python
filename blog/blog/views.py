1f3b9e44eb66cec448dc9099271a228e72887b05
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-05-05 22:02:43 +0400

    blog: add pagination

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -5,8 +5,36 @@ from .database import session
 from .models import Post
 
 @app.route("/")
+@app.route("/page/<int:page>")
+def posts(page=1, paginate_by=10):
+    # Zero-indexed page
+    page_index = page - 1
+    
+    count = session.query(Post).count()
+
+    start = page_index * paginate_by
+    end = start + paginate_by
+
+    total_pages = (count - 1) / paginate_by + 1
+    has_next = page_index < total_pages - 1
+    has_prev = page_index > 0
+
+    posts = session.query(Post)
+    posts = posts.order_by(Post.datetime.desc())
+    posts = posts[start:end]
+
+    return render_template("posts.html",
+        posts=posts,
+        has_next=has_next,
+        has_prev=has_prev,
+        page=page,
+        total_pages=total_pages
+    )
+
+"""
 def posts():
     posts = session.query(Post)
     posts = posts.order_by(Post.datetime.desc())
     posts = posts.all()
     return render_template("posts.html", posts=posts)
+"""
