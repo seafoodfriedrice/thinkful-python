3c3ea645c21801f2602ff5d6f6c29f730be80b0a
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-05-09 23:57:31 +0400

    invoice_calculator: add testDividedFairly() test

--- invoice_calculator/test_invoice_calculator.py
+++ invoice_calculator/test_invoice_calculator.py
@@ -1,7 +1,13 @@
 import unittest
+from invoice_calculator import divide_pay
 
 class InvoiceCalculatorTests(unittest.TestCase):
-    pass
+
+    def testDividedFairly(self):
+        staff_pay = divide_pay(360.0, {"Alice": 3.0, "Bob": 3.0, "Carol": 6.0})
+        self.assertEqual(staff_pay["Alice"], 90.0)
+        self.assertEqual(staff_pay["Bob"], 90.0)
+        self.assertEqual(staff_pay["Carol"], 180.0)
 
 if __name__ == "__main__":
     unittest.main()
