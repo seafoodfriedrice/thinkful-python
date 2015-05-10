aaade1e0559ec27bd4bb274acf59148d5ec6f1d9
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-05-10 21:17:06 +0400

    blog: add TestingConfig class to config.py

--- blog/blog/config.py
+++ blog/blog/config.py
@@ -4,3 +4,8 @@ class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/blogful"
     DEBUG = True
     SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", "Blogful/5432")
+
+class TestingConfig(object):
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/blogful-test"
+    DEBUG = False
+    SECRET_KEY = "Not secret"
