ce91285ddf25bc5fb138d592a5a3b57b12f9a5ef
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-05-11 16:36:30 +0000

    blog: change credentials in SQLALCHEMY_DATABASE_URI

--- blog/blog/config.py
+++ blog/blog/config.py
@@ -1,11 +1,11 @@
 import os
 
 class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/blogful"
+    SQLALCHEMY_DATABASE_URI = "postgresql://action:action@localhost:5432/blogful"
     DEBUG = True
     SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", "Blogful/5432")
 
 class TestingConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/blogful-test"
+    SQLALCHEMY_DATABASE_URI = "postgresql://action:action@localhost:5432/blogful-test"
     DEBUG = False
     SECRET_KEY = "Not secret"
