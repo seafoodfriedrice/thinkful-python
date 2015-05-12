356e1091af6e4c4537d5e043fa9361fb1b93503a
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-05-12 23:25:02 +0400

    blog: combine add post GET and POST functions into single view

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -92,19 +92,18 @@ def edit_post_post(post_id):
     session.commit()
     return redirect(url_for("post", post_id=post_id))
 
-@app.route("/post/add", methods=["GET"])
+@app.route("/post/add", methods=["GET", "POST"])
 @login_required
-def add_post_get():
-    return render_template("add_post.html")
+def add_post():
+    if request.method == "GET":
+        return render_template("add_post.html")
 
-@app.route("/post/add", methods=["POST"])
-@login_required
-def add_post_post():
-    post = Post(
-        title=request.form["title"],
-        content=mistune.markdown(request.form["content"]),
-        author=current_user
-    )
-    session.add(post)
-    session.commit()
-    return redirect(url_for("posts"))
+    if request.method == "POST":
+        post = Post(
+            title=request.form["title"],
+            content=mistune.markdown(request.form["content"]),
+            author=current_user
+        )
+        session.add(post)
+        session.commit()
+        return redirect(url_for("posts"))
